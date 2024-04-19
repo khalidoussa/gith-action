00e3dda4329843c4178651420054e299f678d6af
khalidoussaidi <khalid@fr.fr>
2024-04-19 17:20:08 +0200

    make test fail

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,6 @@ describe('Test suite github action ', () => {
     cy.get('[data-test="username-login"]').type('login_user')
     cy.get('[data-test="password-login"]').type('cypress-geek')
     cy.get('[data-test="submit-login"]').click()
-    cy.get('.MuiPaper-root').should('have.length', 8)
+    cy.get('.MuiPaper-root').should('have.length', 9)
   })
 })
